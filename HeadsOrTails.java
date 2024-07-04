306b91508addfe1b7a2c729053513fc0df9ca205
HOSHINO Tai <s1310194@linsv211.u-aizu.ac.jp>
2024-07-04 14:36:05 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class HeadsOrTails {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int headsCount = 0;
+        int tailscount = 0;
+
+        System.out.println("Tossing a coin...");
+
+        for (int i = 1; i <= 3; i++) {
+            //random.nextBoolean()がtrueのとき、head
+            if (random.nextBoolean()) {
+                System.out.println("Round " + i + ": Heads");
+                headsCount++;
+            }
+            else {
+                System.out.println("Round " + i + ": Tails");
+                tailscount++;
+            }
+        }
+
+        System.out.println("Heads: " + headsCount + ", Tails: " + tailscount);
+    }
+}
